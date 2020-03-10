7f07ce41abeee16ad5a5d58e859d7e684701400e
Avazbek Niyazov <avazbe@gmail.com>
2020-03-10 20:17:47 +0500

    Initial files added

---
+++ precache-manifest.a0a4c6384d25de672f43ce8b9f0a6b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53f4b9dc6bfed5a8a7c133d3d0e7e78c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca27ead689d938375ee",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bac0c7d4be3d4c3ebfe9",
+    "url": "/static/js/2.cf2fa2bf.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.cf2fa2bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca27ead689d938375ee",
+    "url": "/static/js/main.eb037323.chunk.js"
+  },
+  {
+    "revision": "9ba5276b52b16575daed",
+    "url": "/static/js/runtime-main.594cbefe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
